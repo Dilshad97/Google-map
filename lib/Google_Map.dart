408068b3f6ee64d508018b161156cec66966f6f6
Dilshad Alam <rayeendilshan@gmail.com>
2021-09-03 11:58:32 +0530

    google map marker set

--- lib/Google_Map.dart
+++ lib/Google_Map.dart
@@ -12,19 +12,61 @@ class Map_locator extends StatefulWidget {
 }
 
 class _Map_locatorState extends State<Map_locator> {
+  Set<Marker> _markers = Set();
 
   Completer<GoogleMapController> _controller = Completer();
 
+
+//   void OnTap() {
+//     setState(() {
+// _markers=true;
+//
+//     });
+//   }
+
+
+  // @override
+  // void initState() {
+  //   // setState(() {
+  //   //   markers = _markers;
+  //   // });
+  //   super.initState();
+  // }
+
+
   @override
   Widget build(BuildContext context) {
+
+
+    _markers.addAll([
+      // Marker(
+      //     markerId: MarkerId('value'),
+      //     position: LatLng(22.3039, 70.8022)),
+      Marker(
+          markerId: MarkerId('value2'),
+          position: LatLng(23.0225,72.5714)),
+    ]);
+
+
     return SafeArea(child: Scaffold(
 
       body: GoogleMap(
+        // markers: Set<Marker>.of(markers.values),
         mapType: MapType.normal,
         initialCameraPosition: CameraPosition(target: LatLng(23.0225,72.5714) ,zoom: 13),
     onMapCreated: (GoogleMapController controller) {
       _controller.complete(controller);
-    }),
+    },
+        onTap: (LatLng latLng) {
+          _markers.add(Marker(markerId: MarkerId('mark'), position: latLng));
+          setState(() {});
+        } ,
+        markers: Set<Marker>.of(_markers),
+    )
+
+
+
     ));
+
   }
 }
